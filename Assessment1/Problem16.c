9b6e63843f6e2acf70cfeb8fbd67336e944bba81
trivikraman <56528999+trivikraman@users.noreply.github.com>
2025-12-30 00:35:35 +0530

    Update Problem16.c

--- Assessment1/Problem16.c
+++ Assessment1/Problem16.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int a;
-    scanf("%d",&a);
-    printf("%d",((((a%1000)-(a%100))/100))*1000+(((a-(a%1000))/1000)*100)+(a%100));
+    int x,y;
+    printf("Enter Number:");
+    scanf("%d",&x);
+    y=((((x%1000)-(x%100))/100))*1000+(((x-(x%1000))/1000)*100)+(x%100)
+    printf("Result= %d",y);
     return 0;
 }
