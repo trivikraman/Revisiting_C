a026137616559810c46ada588b60c05fe272e392
trivikraman <56528999+trivikraman@users.noreply.github.com>
2025-12-30 01:23:49 +0530

    Create Problem6.c

---
+++ Assessment3/Problem6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+    printf("Enter Number:");
+    scanf("%d",&x);
+    if(x>=50)
+    {
+        printf("Success");
+    }
+    else
+    {
+        printf("Failure");
+    }
+    
+    return 0;
+}
