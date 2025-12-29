5407bbfd8748e55f49365e2cc08f33565668f8f7
trivikraman <56528999+trivikraman@users.noreply.github.com>
2025-12-30 01:05:52 +0530

    Update Problem11.c

--- Assessment2/Problem11.c
+++ Assessment2/Problem11.c
@@ -4,7 +4,7 @@ int main() {
     int x,y;
     printf("Enter Number:");
     scanf("%d",&x);
-    y=((x%10)<=((x-x%10)/10))?1:0;
+    y=(((x-x%10)/10)<=(x%10))?1:0;
     printf("Result= %d",y);
     return 0;
 }
