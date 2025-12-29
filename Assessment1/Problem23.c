d7dc8897e7d1207eebc282a786e8e0a2a4007511
trivikraman <56528999+trivikraman@users.noreply.github.com>
2025-12-30 00:41:07 +0530

    Update Problem23.c

--- Assessment1/Problem23.c
+++ Assessment1/Problem23.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int a;
-    scanf("%d",&a);
-    printf("%d",(a%10+(a-a%10)/10)%2!=0?a-5:a);
+    int x,y;
+    printf("Enter Number:");
+    scanf("%d",&x);
+    y=(x%10+(x-x%10)/10)%2!=0?x-5:x;
+    printf("Result= %d",y);
     return 0;
 }
