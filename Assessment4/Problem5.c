032b9ef44c726412738cc3bb783a539aeab1bb6b
trivikraman <56528999+trivikraman@users.noreply.github.com>
2026-01-13 12:50:22 +0530

    Create Problem5.c

---
+++ Assessment4/Problem5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    int odd;
+    odd=0;
+    x=1;
+    loop:if(x<6)
+    {
+        odd=odd+1;
+        if(odd%2!=0)
+        {
+          printf("%d\n",odd);
+          x=x+1;
+        }
+        goto loop;
+    }
+}
